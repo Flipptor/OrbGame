2468c9f7d92a9cad5499430d75b31bd4f3aad591
Viktor Anderling <natanande@gmail.com>
2013-03-25 19:03:43 +0100

    Fixed screens

--- game/src/com/flipptor/orbgame/OrbGame.java
+++ game/src/com/flipptor/orbgame/OrbGame.java
@@ -1,7 +1,8 @@
 package com.flipptor.orbgame;
 
-import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -9,58 +10,25 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.flipptor.orbgame.screens.GameScreen;
+import com.flipptor.orbgame.screens.SplashScreen;
 
-public class OrbGame implements ApplicationListener {
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
+public class OrbGame extends Game {
+	
+	private Screen guideScreen;
+	private Screen ingameMenu;
+	private Screen gameOverScreen;
+	private Screen mainMenuScreen;
+	
+	protected Screen splashScreen;
+	protected Screen gameScreen;
+	
+	private Screen highscoreScreen;
 	
 	@Override
-	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-	}
-
-	@Override
-	public void dispose() {
-		batch.dispose();
-		texture.dispose();
-	}
-
-	@Override
-	public void render() {		
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-	}
-
-	@Override
-	public void resize(int width, int height) {
-	}
-
-	@Override
-	public void pause() {
-	}
-
-	@Override
-	public void resume() {
+	public void create() {
+		gameScreen = new GameScreen(this);
+		splashScreen = new SplashScreen(this);
+		setScreen(splashScreen);
 	}
 }
---
+++ game/src/com/flipptor/orbgame/screens/GameScreen.java
@@ -0,0 +1,56 @@
+package com.flipptor.orbgame.screens;
+
+import com.badlogic.gdx.Screen;
+import com.flipptor.orbgame.OrbGame;
+
+public class GameScreen implements Screen {
+	
+	OrbGame game;
+	
+	public GameScreen(OrbGame game) {
+		this.game = game;
+	}
+	
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
