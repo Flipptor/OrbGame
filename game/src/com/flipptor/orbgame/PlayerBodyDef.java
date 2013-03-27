c82497cbe60d8e59e76c58ab193456d5da1117cb
Viktor Anderling <natanande@gmail.com>
2013-03-27 15:08:48 +0100

    added a body def for the player

---
+++ game/src/com/flipptor/orbgame/PlayerBodyDef.java
@@ -0,0 +1,23 @@
+package com.flipptor.orbgame;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+
+/**
+ * 
+ * @author Viktor Anderling
+ *
+ * A body definition for the player's body.
+ * 
+ */
+public class PlayerBodyDef extends BodyDef {
+	
+	public PlayerBodyDef(Vector2 position) {
+		super();
+		this.type = BodyType.DynamicBody;
+		this.allowSleep = false;
+		this.fixedRotation = true;
+		this.linearDamping = 0f;
+		this.position.set(position);
+	}
+}
