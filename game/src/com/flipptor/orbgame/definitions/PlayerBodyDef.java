922b696162c278f75251c373ceaae173527cdcf3
Viktor Anderling <natanande@gmail.com>
2013-03-27 15:33:19 +0100

    created a fixture def for player and moved all defs to new package

--- game/src/com/flipptor/orbgame/definitions/PlayerBodyDef.java
+++ game/src/com/flipptor/orbgame/definitions/PlayerBodyDef.java
@@ -1,4 +1,4 @@
-package com.flipptor.orbgame;
+package com.flipptor.orbgame.definitions;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
---
+++ game/src/com/flipptor/orbgame/definitions/PlayerFixtureDef.java
@@ -0,0 +1,18 @@
+package com.flipptor.orbgame.definitions;
+
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+
+public class PlayerFixtureDef extends FixtureDef {
+
+	public PlayerFixtureDef INSTANCE = new PlayerFixtureDef();
+	
+	private PlayerFixtureDef() {
+		// TODO set these variables to better values.
+		this.shape = new CircleShape();
+		this.shape.setRadius(1f);
+		this.density = 0;
+		this.friction = 0;
+		this.restitution = 0;
+	}
+}
