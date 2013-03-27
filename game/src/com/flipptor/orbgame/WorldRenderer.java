3e44391de8a4916e9f3e11427e6c0c691f6068a4
John Eriksson <yanis.cool3@gmail.com>
2013-03-27 18:43:28 +0100

    Added non-controllable player and lights

--- game/src/com/flipptor/orbgame/WorldRenderer.java
+++ game/src/com/flipptor/orbgame/WorldRenderer.java
@@ -1,8 +1,10 @@
 package com.flipptor.orbgame;
 
+import box2dLight.ConeLight;
 import box2dLight.RayHandler;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
@@ -10,6 +12,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.flipptor.orbgame.definitions.PlayerBodyDef;
+import com.flipptor.orbgame.definitions.PlayerFixtureDef;
 
 public class WorldRenderer {
 	World world;
@@ -25,8 +28,8 @@ public class WorldRenderer {
 	public WorldRenderer(OrbGame game) {
 		this.game = game;
 		
-		width = Gdx.graphics.getWidth();
-		height = Gdx.graphics.getHeight();
+		width = Gdx.graphics.getWidth()/10;
+		height = Gdx.graphics.getHeight()/10;
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		
 		
@@ -41,13 +44,17 @@ public class WorldRenderer {
 		renderer = new Box2DDebugRenderer();
 		handler = new RayHandler(world);
 		handler.setCombinedMatrix(camera.combined);
+		
+		createPlayer();
+		new ConeLight(handler, 40000, Color.GRAY, 150, width/2, height, -90, 35);
+		
 	}
 	
 	public void render(){
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
 		renderer.render(world, camera.combined);
-		renderer.setDrawBodies(true);
+		renderer.setDrawBodies(false);
 		handler.update();
 		handler.render();
 		world.step(1/60f, 6, 2);
@@ -55,9 +62,6 @@ public class WorldRenderer {
 	
 	public void createPlayer(){
 		p = new PlayerEntity(world, new PlayerBodyDef(new Vector2(width/2, height/2)));
-		pb = p.getBody();
-		
-		
+		p.body.createFixture(PlayerFixtureDef.INSTANCE);
 	}
-	
 }
