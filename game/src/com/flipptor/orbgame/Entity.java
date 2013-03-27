adee7ebc9a48a19d293e71bea774f7ae8b791f4d
John Eriksson <yanis.cool3@gmail.com>
2013-03-27 15:36:21 +0100

    Created Entity class for bodies

---
+++ game/src/com/flipptor/orbgame/Entity.java
@@ -0,0 +1,29 @@
+package com.flipptor.orbgame;
+
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * 
+ * @author John Eriksson
+ *
+ * Main framework for an Entity.
+ *
+ */
+
+public abstract class Entity {
+	
+	public Body body;
+	
+	public Entity(World world, BodyDef bodyDef) {
+		body = world.createBody(bodyDef);
+		
+	}
+	
+	public Body getBody() {
+		return body;
+	}
+	
+	
+}
