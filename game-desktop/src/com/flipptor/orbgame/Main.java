bb92bec29cb53d6e192d6c70ea81d44cfa123456
John Eriksson <yanis.cool3@gmail.com>
2013-03-27 20:47:25 +0100

    Added dynamic resolution

--- game-desktop/src/com/flipptor/orbgame/Main.java
+++ game-desktop/src/com/flipptor/orbgame/Main.java
@@ -1,15 +1,23 @@
 package com.flipptor.orbgame;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Main {
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private static double height = screenSize.getHeight();
+	private static double width = screenSize.getWidth();
+	
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "game";
 		cfg.useGL20 = true;
-		cfg.width = 1280;
-		cfg.height = 720;
+		cfg.width = (int) width;
+		cfg.height = (int) height;
+		
 		
 		new LwjglApplication(new OrbGame(), cfg);
 	}
