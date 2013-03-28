2d8bb21310be2dfd767f10ba50caed4615e9848f
Viktor Anderling <natanande@gmail.com>
2013-03-28 20:18:13 +0100

    Added enemyentity and entityhandler

---
+++ game/src/com/flipptor/orbgame/EnemyEntity.java
@@ -0,0 +1,13 @@
+package com.flipptor.orbgame;
+
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+
+public class EnemyEntity extends Entity {
+
+	public EnemyEntity(World world, BodyDef bodyDef) {
+		super(world, bodyDef);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ game/src/com/flipptor/orbgame/EntityHandler.java
@@ -0,0 +1,48 @@
+package com.flipptor.orbgame;
+
+import java.util.LinkedList;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import com.flipptor.orbgame.definitions.EnemyBodyDef;
+import com.flipptor.orbgame.definitions.EnemyFixtures;
+import com.flipptor.orbgame.definitions.PlayerBodyDef;
+import com.flipptor.orbgame.definitions.PlayerFixtureDef;
+
+public class EntityHandler {
+	private static final float WIDTH = Gdx.graphics.getWidth()/10;
+	private static final float HEIGHT = Gdx.graphics.getHeight()/10;
+	
+	private LinkedList<Entity> enemyList, creditList;
+	private PlayerEntity player;
+	private float dX, dY;
+	//private World world;
+	
+	EnemyEntity enemy;
+	
+	public EntityHandler(World world) {
+		enemyList = new LinkedList<Entity>();
+		creditList = new LinkedList<Entity>();
+		player = new PlayerEntity(world, 
+				new PlayerBodyDef(new Vector2(WIDTH/2, HEIGHT/2)));
+		player.body.createFixture(PlayerFixtureDef.INSTANCE);
+		enemy = new EnemyEntity(world, new EnemyBodyDef(new Vector2(WIDTH*1.3f/2, HEIGHT*1.3f/2)));
+		enemy.body.createFixture(EnemyFixtures.MEDIUM.fixtureDef);
+	}
+	
+	public void update() {
+		moveEntities();
+		player.update();
+	}
+	
+	private void moveEntities() {
+		Vector2 pos = player.getBody().getPosition();
+		dX = pos.x - WIDTH/2;
+		dY = pos.y - HEIGHT/2;
+		player.getBody().setTransform(WIDTH/2, HEIGHT/2, 0);
+		enemy.getBody().setTransform(enemy.getBody().getPosition().x - dX,
+				enemy.getBody().getPosition().y - dY, 0);
+		
+	}
+}
