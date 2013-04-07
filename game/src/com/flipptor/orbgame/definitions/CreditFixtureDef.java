cbd1046fe8e017dcd4c8bd187f3bb100c6169b20
Viktor Anderling <natanande@gmail.com>
2013-04-07 21:01:50 +0200

    changed constructor for CreditFixtureDef

--- game/src/com/flipptor/orbgame/definitions/CreditFixtureDef.java
+++ game/src/com/flipptor/orbgame/definitions/CreditFixtureDef.java
@@ -5,13 +5,13 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class CreditFixtureDef extends FixtureDef {
 
-	public CreditFixtureDef(float radius) {
+	public CreditFixtureDef() {
 		super();
 		this.shape = new CircleShape();
 		this.density = 0;
 		this.friction = 0;
 		this.restitution = 0;
-		this.shape.setRadius(radius);
+		this.shape.setRadius(0.8f);
 		this.isSensor = true;
 	}
 }
