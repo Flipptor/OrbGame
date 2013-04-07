1c144e990e95e1fe9fd0b130b5c48df5e58fee0d
Viktor Anderling <natanande@gmail.com>
2013-04-07 22:07:46 +0200

    Added methods for health and credits to the player

--- game/src/com/flipptor/orbgame/PlayerEntity.java
+++ game/src/com/flipptor/orbgame/PlayerEntity.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.physics.box2d.World;
 
 public class PlayerEntity extends Entity {
 	
+	private int credits = 0;
 	private float health = 3;
 	private IGameInput input;
 	private Vector2 movementForce;
@@ -37,5 +38,69 @@ public class PlayerEntity extends Entity {
 			dashForce.set(dashForce.x*dashMultiplier, dashForce.y*dashMultiplier);
 			this.getBody().applyForceToCenter(dashForce);
 		}
+	}
+	
+	/**
+	 * Adds a number of credits to the player.
+	 * 
+	 * @param value The number of credits to be added.
+	 */
+	public void addCredits(int value) {
+		credits += value;
+		// TODO maybe increase score here, or maybe in EntityHandler?
+	}
+	
+	/**
+	 * Removes a number of credits from the player.
+	 * If the players total credits is less than the amount removed, it will be
+	 * reduced to zero instead.
+	 * 
+	 * @param value The number of credits to be removed.
+	 */
+	public void removeCredits(int value) {
+		if(value >= credits) {
+			credits = 0;
+		} else {
+			credits -= value;
+		}
+	}
+	
+	/**
+	 * @return The players total amount of credits.
+	 */
+	public int getCredits() {
+		return credits;
+	}
+	
+	/**
+	 * Inflicts damage on the player.
+	 * 
+	 * @param damage The amount of damage to be inflicted.
+	 */
+	public void takeDamage(float damage) {
+		health -= damage;
+	}
+	
+	/**
+	 * Heals an amount of damage from the player.
+	 * 
+	 * @param health The amount to be healed.
+	 */
+	public void healDamage(float health) {
+		this.health += health;
+	}
+	
+	/**
+	 * Heals all damage from the player, setting its health to full.
+	 */
+	public void healAllDamage() {
+		// TODO not able to implement yet...
+	}
+	
+	/**
+	 * @return The amount of health left of the player.
+	 */
+	public float getHealth() {
+		return health;
 	}
 }
