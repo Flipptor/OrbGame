379b8152de0c63f7eaef6e944e7e0aa5621a67d3
Viktor Anderling <natanande@gmail.com>
2013-04-13 18:20:36 +0200

    Tweaked feel of game. Added a max speed that depends on if the player is dashing

--- game/src/com/flipptor/orbgame/entities/PlayerEntity.java
+++ game/src/com/flipptor/orbgame/entities/PlayerEntity.java
@@ -3,6 +3,7 @@ package com.flipptor.orbgame.entities;
 import box2dLight.PointLight;
 import box2dLight.RayHandler;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
@@ -14,13 +15,17 @@ import com.flipptor.orbgame.definitions.PlayerBodyDef;
 
 public class PlayerEntity extends Entity {
 	
+	private static final float MAX_SPEED = 20;
+	private static final float DASH_TIME = 1f; // in seconds.
+	private float dashTimeLeft;
+	
 	private int credits = 0;
 	private Status status;
 	private IGameInput input;
 	private Vector2 movementForce;
 	private Vector2 dashForce;
 	private static final float MOVEMENT_MULTIPLIER = 10;
-	private float dashMultiplier = 100000;
+	private float dashMultiplier = 1000;
 	
 	public PlayerEntity(World world, Vector2 position, RayHandler rayHandler) {
 		super(world, new PlayerBodyDef(position), 
@@ -33,17 +38,35 @@ public class PlayerEntity extends Entity {
 	public void update() {
 		input.update();
 		status.update();
-		movementForce = input.getMovementVector();
-		movementForce.set(movementForce.x*MOVEMENT_MULTIPLIER, 
-				movementForce.y*MOVEMENT_MULTIPLIER);
-		this.getBody().applyForceToCenter(movementForce);
-		if(input.isDashing()) {
-			dashForce = input.getDashVector();
-			dashForce.set(dashForce.x*dashMultiplier, dashForce.y*dashMultiplier);
-			this.getBody().applyForceToCenter(dashForce);
-		}
+		updateMovement();
 	}
 	
+	private void updateMovement() {
+		// Update dash timing.
+		if(dashTimeLeft != 0) {
+			dashTimeLeft -= Gdx.graphics.getDeltaTime();
+			if(dashTimeLeft < 0) {
+				dashTimeLeft = 0;
+			}
+		}
+		Vector2 speed = this.getBody().getLinearVelocity();
+		if(speed.len() > getCurrentMaxSpeed()) {
+			this.getBody().setLinearVelocity(speed.limit(getCurrentMaxSpeed()));
+		}
+		if(input.isDashing()) {
+			this.getBody().setLinearVelocity(0, 0); // Stops the entity.
+			dashForce = input.getDashVector().scl(dashMultiplier);
+			this.getBody().applyForceToCenter(dashForce);
+			dashTimeLeft = DASH_TIME;
+		}
+		movementForce = input.getMovementVector().scl(MOVEMENT_MULTIPLIER);
+		this.getBody().applyForceToCenter(movementForce);
+	}
+	
+	private float getCurrentMaxSpeed() {
+		return MAX_SPEED + (dashTimeLeft/DASH_TIME)*MAX_SPEED*2;
+	}
+
 	/**
 	 * Adds a number of credits to the player.
 	 * 
@@ -81,5 +104,12 @@ public class PlayerEntity extends Entity {
 	 */
 	public Status getStatus() {
 		return this.status;
+	}
+	
+	/**
+	 * @return true if the player is currently dashing, false otherwise.
+	 */
+	public boolean isDashing() {
+		return dashTimeLeft != 0;
 	}
 }
